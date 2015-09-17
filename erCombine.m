57db93d49487ce7e98fddcc5a0c6e4821cc19a7c
money999 <foney939@gmail.com>
2015-09-17 10:51:49 +0800

    增加

--- erCombine.m
+++ erCombine.m
@@ -1,4 +1,4 @@
-function [ mpvc1, mpvc0, mpvcA ] = erCombine( pos1, vel1, cla1, pos2, vel2, cla2 , p_p, p_r, v_p, v_r, wAttri )
+function [ alpha1, alpha0, alphaA ] = erCombine( pos1, vel1, cla1, pos2, vel2, cla2 , p_p, p_r, v_p, v_r, wAttri )
 %UNTITLED 此处显示有关此函数的摘要
 %   wAttri 属性权重3-by-1，权重顺序是pvc
 %   此处显示详细说明
@@ -30,24 +30,36 @@ mcA = 1 - mc0;
 
 %mp1,mp0,mpA,    mv0,mvA,     mc0,mcA;
 %将原来算好的BPA乘上权重
-% m~H -> mw
-% m-H -> mu
+% m~H -> mwH
+% m-H -> muH
 mH1p = mp1 * wAttri(1);
 mH0p = mp0 * wAttri(1);
-muHp = 1 - wAttri(1);
+muHp = (1 - wAttri(1)) * ones(size(mp0));
 mwHp = mpA * wAttri(1)
 
 %mH1v = mv1 * wAttri(2);mv1是0不用
 mH0v = mv0 * wAttri(2);
-muHv = 1 - wAttri(2);
+muHv = (1 - wAttri(2)) * ones(size(mv0));
 mwHv = mvA * wAttri(1);
 
 %mH1c = mc1 * wAttri(3);
 mH0c = mc0 * wAttri(3)
-muHv = 1 - wAttri(3);
-mwHv = mcA * wAttri(3);
+muHc = (1 - wAttri(3)) * ones(size(mc0));
+mwHc = mcA * wAttri(3);
 
+muH = mwHp.*mwHv.*mwHc;
+mwH = (muHp + mwHp).*(muHv + mwHv).*(muHc + mwHc) - muH;
+mH1 = (mH1p + muHp + mwHp).*(muHv + mwHv).*(muHc + mwHc) - (mwH + muH);
+mH0 = (mH0p + muHp + mwHp).*(mH0v + muHv + mwHv).*(mH0c + muHc + mwHc) - (mwH + muH);
+k = muH + mwH + mH1 + mH0;
+mH1 = mH1./k;
+mH0 = mH0./k;
+mwH = mwH./k;
+muH = muH./k;
 
+alpha0 = mH0./(1 - muH);
+alpha1 = mH1./(1 - muH);
+alphaA = mwH./(1 - muH);
 
 end
 
