bd1c282298ecfa0651ccf69393b8671bf6cab2ce
money999 <foney939@gmail.com>
2015-10-02 18:34:26 +0800

    [x,y]=meshgrid([-5:5],[-5:5]); X = [x(:) y(:)];

--- testfun.m
+++ testfun.m
@@ -1,5 +1,12 @@
 function y = testfun(x)
-y = -x(1) * x(2) * x(3);
+%y = -x(1) * x(2) * x(3);
+%y = sqrt( (  sin(x(1)) * x(3) - sin(x(2)) * x(4) )^2 + (cos(x(1))*x(3) - cos(x(2)) * x(4) )^2);
+x1 = cos(x(1)) * x(3);
+x2 = cos(x(2)) * x(4);
+y1 = sin(x(1)) * x(3);
+y2 = sin(x(2)) * x(4);
 
+y = -pdist2([x1, y1], [x2, y2]);
+%y = sin(x(1))*x(3) + cos(x(2))*x(4);
 end
 
